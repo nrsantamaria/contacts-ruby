1d8cff6f37d8f7afe8f7a91ec13c1f2063875d71
Nicole Santamaria and Sean Pierce <sean@>
2017-04-27 11:01:51 -0700

    create pages in views folder. create paths between all pages in app.rb

--- app.rb
+++ app.rb
@@ -8,3 +8,15 @@ also_reload('lib/**/*.rb')
 get('/') do
   erb(:index)
 end
+
+get('/contacts') do
+  erb(:contact_list)
+end
+
+get('/add_contact') do
+  erb(:add_contact_form)
+end
+
+get('/contact/:id') do
+  erb(:contact_details)
+end
